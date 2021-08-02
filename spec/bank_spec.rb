0229d7e5d0c575d7d6747fba977b69121fa75c90
Ashley Slaney <ashleyslaney96@gmail.com>
2021-08-02 11:21:57 +0100

    can deposit money

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -6,4 +6,9 @@ describe Bank do
   it 'starts with 0 balance' do
     expect(bank.balance).to eq(0)
   end
+
+  it 'can deposit money' do
+    bank.deposit(100)
+    expect(bank.balance).to eq(100)
+  end
 end
