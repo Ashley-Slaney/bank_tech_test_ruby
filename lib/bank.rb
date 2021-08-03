afb30f07520d2afd765ef4b37d9d1d57a890b15f
Ashley Slaney <ashleyslaney96@gmail.com>
2021-08-03 16:53:20 +0100

    added method comments

--- lib/bank.rb
+++ lib/bank.rb
@@ -6,12 +6,14 @@ class Bank
     @transactions = []
   end
 
+  # deposit money
   def deposit(amount)
     @balance += amount
     transaction("#{'%.2f' % amount} ||")
     @balance
   end
 
+  # withdraw money - won't allow you to go below 0
   def withdraw(amount)
     if @balance - amount < 0
       return "Your balance can not go below 0"
@@ -22,6 +24,7 @@ class Bank
     end
   end
   
+  # responsible for outputting each transaction 
   def statement
     reverse_transactions
     
@@ -34,14 +37,17 @@ class Bank
   
   private 
   
+  # pushes each transaction to the array that all of the transactions are being stored in
   def transaction(amount)
     @transactions.push(date: Time.now.strftime("%d/%m/%Y"), amount: amount, balance: @balance)
   end
 
+  # shows the most recent transactions first
   def reverse_transactions
     @statement_output = "date || credit || debit || balance"
     @transactions.reverse.each do |transaction|
       @statement_output << "\n#{transaction[:date]} || #{transaction[:amount]} || #{'%.2f' % transaction[:balance]}"
     end
   end
+
 end
