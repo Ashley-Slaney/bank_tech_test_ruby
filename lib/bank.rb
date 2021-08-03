c637f5864b9cefe8b68230cee73baefb264f7395
Ashley Slaney <ashleyslaney96@gmail.com>
2021-08-03 15:42:09 +0100

    can see the bank statement

--- lib/bank.rb
+++ lib/bank.rb
@@ -3,13 +3,34 @@ class Bank
 
   def initialize
     @balance = 0
+    @transactions = []
+    @statement_output = "date || credit || debit || balance"
   end
 
   def deposit(amount)
     @balance += amount
+    transaction("#{'%.2f' % amount} ||")
   end
 
   def withdraw(amount)
-    @balance -= amount
+    @balance -= (amount)
+    transaction("|| #{'%.2f' % amount}")
+  end
+
+  def transaction(amount)
+    @transactions.push(date: Time.now.strftime("%d/%m/%Y"), amount: amount, balance: @balance)
+  end
+
+  def statement
+    @transactions.reverse.each do |transaction|
+      @statement_output << "\n#{transaction[:date]} || #{transaction[:amount]} || #{'%.2f' % transaction[:balance]}"
+    end
+
+    output = @statement_output.split("\n")
+    output.each do |transaction|
+      puts transaction
+    end
+
+    @statement_output
   end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -2,19 +2,32 @@ require 'bank'
 
 describe Bank do
   let(:bank) { Bank.new }
+  let(:date) { Time.now.strftime("%d/%m/%Y") }
 
   it 'starts with 0 balance' do
-    expect(bank.balance).to eq(0)
+    expect(bank.balance).to eq(0.00)
   end
 
   it 'can deposit money' do
     bank.deposit(100)
-    expect(bank.balance).to eq(100)
+    expect(bank.balance).to eq(100.00)
   end
 
   it 'can withdraw money' do
-    bank.deposit(100)
-    bank.withdraw(50)
-    expect(bank.balance).to eq(50)
+    bank.deposit(1000)
+    bank.deposit(2000)
+    bank.withdraw(500)
+    expect(bank.balance).to eq(2500.00)
+  end
+
+  it 'prints the statement' do
+    expect(bank.statement).to eq("date || credit || debit || balance")
+  end
+
+  it 'prints the statement after some transactions' do
+    bank.deposit(1000)
+    bank.deposit(2000)
+    bank.withdraw(500)
+    expect(bank.statement).to eq("date || credit || debit || balance\n03/08/2021 || || 500.00 || 2500.00\n03/08/2021 || 2000.00 || || 3000.00\n03/08/2021 || 1000.00 || || 1000.00")
   end
 end
