628efc6280cd57508a5e7e15d60d25b639772d40
Ashley Slaney <ashleyslaney96@gmail.com>
2021-08-05 16:54:07 +0100

    can print a statement

--- lib/statement.rb
+++ lib/statement.rb
@@ -5,9 +5,27 @@ class Statement
     @transactions = []
   end
 
+  def print_statement
+    organise_transactions
+    
+    output = @statement_output.split("\n")
+    output.each do |transaction|
+      puts transaction
+    end
+    @statement_output
+  end
+
   private
-  
+
   def store_transaction(amount, balance)
     @transactions.push(date: Time.now.strftime("%d/%m/%Y"), amount: amount, balance: balance)
   end
+
+  def organise_transactions
+    @statement_output = "date || credit || debit || balance"
+    @transactions.reverse.each do |transaction|
+      @statement_output << "\n#{transaction[:date]} || #{transaction[:amount]} || #{'%.2f' % transaction[:balance]}"
+    end
+  end
+
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -11,6 +11,13 @@ describe Statement do
   it 'stores multiple transactions' do
     statement.send(:store_transaction, 100, 100)
     statement.send(:store_transaction, 400, 500)
-    expect(statement.transactions).to eq([{date: date, amount: 100, balance: 100}, {date: date, amount: 400, balance: 500}])
+    statement.send(:store_transaction, 500, 1000)
+    expect(statement.transactions).to eq([{date: date, amount: 100, balance: 100},
+                                          {date: date, amount: 400, balance: 500},
+                                          {date: date, amount: 500, balance: 1000}])
+  end
+
+  it 'prints the statement' do
+    expect(statement.print_statement).to eq("date || credit || debit || balance")
   end
 end
