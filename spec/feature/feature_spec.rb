d8fbf884030c25b43f41b801fef684c538473ff2
Ashley Slaney <ashleyslaney96@gmail.com>
2021-08-05 18:19:24 +0100

    feature tests for bank statement

--- spec/feature/feature_spec.rb
+++ spec/feature/feature_spec.rb
@@ -22,4 +22,27 @@ describe Bank do
   it 'can withdraw money and return the balance' do
     expect(bank.deposit(75.23)).to eq(75.23)
   end
+
+  it 'returns the correct balance after random transactions' do
+    bank.deposit(76.25)
+    bank.deposit(5467.53)
+    bank.withdraw(2347.89)
+    expect(bank.balance).to eq(3195.89)
+  end
+
+  it 'displays the bank statement' do
+    expect{ bank.print_statement }.to output("date || credit || debit || balance\n").to_stdout
+  end
+
+  it 'displays the bank statement after some transactions' do
+    bank.deposit(100)
+    bank.deposit(1000)
+    bank.withdraw(100)
+    expect{ bank.print_statement }.to output("date || credit || debit || balance\n05/08/2021 || || 100.00 || 1000.00\n05/08/2021 || 1000.00 || || 1100.00\n05/08/2021 || 100.00 || || 100.00\n").to_stdout
+  end
+
+  it 'does not add a failed transaction' do
+    bank.withdraw(100)
+    expect{ bank.print_statement }.to output("date || credit || debit || balance\n").to_stdout
+  end
 end
