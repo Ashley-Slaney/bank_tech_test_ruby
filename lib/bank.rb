c3a15c4d45f726cb52210865272bdbfc66d11a37
Ashley Slaney <ashleyslaney96@gmail.com>
2021-08-05 17:46:41 +0100

    removed unnecessary comments and added print method to bank class

--- lib/bank.rb
+++ lib/bank.rb
@@ -15,54 +15,15 @@ class Bank
     balance
   end
 
-  # updates the balance with the depositted amount - needs a minimum of £1 to deposit
-  # and calls the transaction method to store the transaction
-
   def withdraw(amount)
     balance = @account.withdraw(amount)
     @statement.store_transaction("|| #{'%.2f' % amount}", balance)
     balance
   end
 
-  # updates the balance with the withdrawn amount - won't allow you to go below 0 and calls
-  # the transaction method to store the transaction
-
-  # def withdraw(amount)
-  #   if @balance - amount < 0
-  #     return "Your balance can not go below 0"
-  #   end
-  #   @balance -= (amount)
-  #   transaction("|| #{'%.2f' % amount}")
-  #   @balance
-  # end
-  
-  # responsible for outputting each transaction 
-
-  # def statement
-  #   organise_transactions
-    
-  #   output = @statement_output.split("\n")
-  #   output.each do |transaction|
-  #     puts transaction
-  #   end
-  #   @statement_output
-  # end
-  
-  # private 
-  
-  # pushes each transaction to the array that all of the transactions are being stored in
-
-  # def transaction(amount)
-  #   @transactions.push(date: Time.now.strftime("%d/%m/%Y"), amount: amount, balance: @balance)
-  # end
-
-  # shows the most recent transactions first
-
-  # def organise_transactions
-  #   @statement_output = "date || credit || debit || balance"
-  #   @transactions.reverse.each do |transaction|
-  #     @statement_output << "\n#{transaction[:date]} || #{transaction[:amount]} || #{'%.2f' % transaction[:balance]}"
-  #   end
-  # end
+  def print_statement
+    @statement.print_statement
+    @account.balance
+  end
 
 end
