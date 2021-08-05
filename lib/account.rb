bc241919752139ae01942032ace2f094452ddef7
Ashley Slaney <ashleyslaney96@gmail.com>
2021-08-05 15:39:59 +0100

    added comments to account methods

--- lib/account.rb
+++ lib/account.rb
@@ -5,6 +5,8 @@ class Account
     @balance = 0.00
   end
 
+  # updates the balance with the depositted amount - needs a minimum of £1 to deposit
+  # and calls the transaction method to store the transaction
   def deposit(amount)
     if amount < 1
       return "Minimum deposit amount: £1"
@@ -12,6 +14,8 @@ class Account
     @balance += amount
   end
 
+  # updates the balance with the withdrawn amount - won't allow you to go below 0 and calls
+  # the transaction method to store the transaction
   def withdraw(amount)
     if @balance - amount < 0
       return "Can not go below £0 balance"
