1fb25330fcce9e18a48fb00f36e281f4ba97d578
Ashley Slaney <ashleyslaney96@gmail.com>
2021-08-05 17:07:14 +0100

    removed unused tests

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -14,22 +14,4 @@ describe Bank do
     expect(statement.transactions).to eq([])
   end
 
-  # it 'prints the statement' do
-  #   expect(bank.statement).to eq("date || credit || debit || balance")
-  # end
-
-  # it 'prints the statement after some transactions' do
-  #   bank.deposit(1000)
-  #   bank.deposit(2000)
-  #   bank.withdraw(500)
-  #   expect(bank.statement).to eq("date || credit || debit || balance\n#{date} || || 500.00 || 2500.00\n#{date} || 2000.00 || || 3000.00\n#{date} || 1000.00 || || 1000.00")
-  # end
-
-  # it 'does not duplicate the statement when calling for the statement consecutively' do
-  #   bank.deposit(1000)
-  #   bank.deposit(500)
-  #   bank.statement
-  #   expect(bank.statement).to eq("date || credit || debit || balance\n#{date} || 500.00 || || 1500.00\n#{date} || 1000.00 || || 1000.00")
-  # end
-
 end
