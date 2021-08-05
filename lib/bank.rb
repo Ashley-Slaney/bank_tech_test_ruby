fe7b80578681fb8d496cdaa32328c712173bdf3a
Ashley Slaney <ashleyslaney96@gmail.com>
2021-08-05 15:26:44 +0100

    added test double for account

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,57 +1,63 @@
 class Bank 
-  attr_reader :balance
+  attr_reader :balance, :account
 
   def initialize
-    @balance = 0
-    @transactions = []
+    @account = Account.new
+    # @balance = 0
+    # @transactions = []
   end
 
   # updates the balance with the depositted amount - needs a minimum of £1 to deposit
   # and calls the transaction method to store the transaction
-  def deposit(amount)
-    if amount < 1
-      return "Minimum deposit amount: £1"
-    end
-    @balance += amount
-    transaction("#{'%.2f' % amount} ||")
-    @balance
-  end
+
+  # def deposit(amount)
+  #   if amount < 1
+  #     return "Minimum deposit amount: £1"
+  #   end
+  #   @balance += amount
+  #   transaction("#{'%.2f' % amount} ||")
+  #   @balance
+  # end
 
   # updates the balance with the withdrawn amount - won't allow you to go below 0 and calls
   # the transaction method to store the transaction
-  def withdraw(amount)
-    if @balance - amount < 0
-      return "Your balance can not go below 0"
-    end
-    @balance -= (amount)
-    transaction("|| #{'%.2f' % amount}")
-    @balance
-  end
+
+  # def withdraw(amount)
+  #   if @balance - amount < 0
+  #     return "Your balance can not go below 0"
+  #   end
+  #   @balance -= (amount)
+  #   transaction("|| #{'%.2f' % amount}")
+  #   @balance
+  # end
   
   # responsible for outputting each transaction 
-  def statement
-    organise_transactions
+
+  # def statement
+  #   organise_transactions
     
-    output = @statement_output.split("\n")
-    output.each do |transaction|
-      puts transaction
-    end
-    @statement_output
-  end
+  #   output = @statement_output.split("\n")
+  #   output.each do |transaction|
+  #     puts transaction
+  #   end
+  #   @statement_output
+  # end
   
-  private 
+  # private 
   
   # pushes each transaction to the array that all of the transactions are being stored in
-  def transaction(amount)
-    @transactions.push(date: Time.now.strftime("%d/%m/%Y"), amount: amount, balance: @balance)
-  end
+
+  # def transaction(amount)
+  #   @transactions.push(date: Time.now.strftime("%d/%m/%Y"), amount: amount, balance: @balance)
+  # end
 
   # shows the most recent transactions first
-  def organise_transactions
-    @statement_output = "date || credit || debit || balance"
-    @transactions.reverse.each do |transaction|
-      @statement_output << "\n#{transaction[:date]} || #{transaction[:amount]} || #{'%.2f' % transaction[:balance]}"
-    end
-  end
+
+  # def organise_transactions
+  #   @statement_output = "date || credit || debit || balance"
+  #   @transactions.reverse.each do |transaction|
+  #     @statement_output << "\n#{transaction[:date]} || #{transaction[:amount]} || #{'%.2f' % transaction[:balance]}"
+  #   end
+  # end
 
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -3,46 +3,53 @@ require 'bank'
 describe Bank do
   let(:bank) { Bank.new }
   let(:date) { Time.now.strftime("%d/%m/%Y") }
+  let(:account) { double("Account", balance: 0) }
 
-  it 'starts with 0 balance' do
-    expect(bank.balance).to eq(0.00)
+  it 'initialises the account class with 0 balance' do
+    expect(account.balance).to eq(0)
   end
 
-  it 'can deposit money' do
-    bank.deposit(100)
-    expect(bank.balance).to eq(100.00)
-  end
 
-  it 'can withdraw money' do
-    bank.deposit(1000)
-    bank.deposit(2000)
-    bank.withdraw(500)
-    expect(bank.balance).to eq(2500.00)
-  end
 
-  it 'prints the statement' do
-    expect(bank.statement).to eq("date || credit || debit || balance")
-  end
+  # it 'starts with 0 balance' do
+  #   expect(bank.balance).to eq(0.00)
+  # end
 
-  it 'prints the statement after some transactions' do
-    bank.deposit(1000)
-    bank.deposit(2000)
-    bank.withdraw(500)
-    expect(bank.statement).to eq("date || credit || debit || balance\n#{date} || || 500.00 || 2500.00\n#{date} || 2000.00 || || 3000.00\n#{date} || 1000.00 || || 1000.00")
-  end
+  # it 'can deposit money' do
+  #   bank.deposit(100)
+  #   expect(bank.balance).to eq(100.00)
+  # end
 
-  it 'can not go below 0 balance' do
-    expect(bank.withdraw(50)).to eq("Your balance can not go below 0")
-  end
+  # it 'can withdraw money' do
+  #   bank.deposit(1000)
+  #   bank.deposit(2000)
+  #   bank.withdraw(500)
+  #   expect(bank.balance).to eq(2500.00)
+  # end
 
-  it 'does not duplicate the statement when calling for the statement consecutively' do
-    bank.deposit(1000)
-    bank.deposit(500)
-    bank.statement
-    expect(bank.statement).to eq("date || credit || debit || balance\n#{date} || 500.00 || || 1500.00\n#{date} || 1000.00 || || 1000.00")
-  end
+  # it 'prints the statement' do
+  #   expect(bank.statement).to eq("date || credit || debit || balance")
+  # end
 
-  it 'needs a minimum of £1 to deposit money' do
-    expect(bank.deposit(0.73)).to eq("Minimum deposit amount: £1")
-  end
+  # it 'prints the statement after some transactions' do
+  #   bank.deposit(1000)
+  #   bank.deposit(2000)
+  #   bank.withdraw(500)
+  #   expect(bank.statement).to eq("date || credit || debit || balance\n#{date} || || 500.00 || 2500.00\n#{date} || 2000.00 || || 3000.00\n#{date} || 1000.00 || || 1000.00")
+  # end
+
+  # it 'can not go below 0 balance' do
+  #   expect(bank.withdraw(50)).to eq("Your balance can not go below 0")
+  # end
+
+  # it 'does not duplicate the statement when calling for the statement consecutively' do
+  #   bank.deposit(1000)
+  #   bank.deposit(500)
+  #   bank.statement
+  #   expect(bank.statement).to eq("date || credit || debit || balance\n#{date} || 500.00 || || 1500.00\n#{date} || 1000.00 || || 1000.00")
+  # end
+
+  # it 'needs a minimum of £1 to deposit money' do
+  #   expect(bank.deposit(0.73)).to eq("Minimum deposit amount: £1")
+  # end
 end
