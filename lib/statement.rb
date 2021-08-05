aeeb076a423a68349834af2cf62baf21cae93f0b
Ashley Slaney <ashleyslaney96@gmail.com>
2021-08-05 16:35:55 +0100

    can store multiple transactions

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,7 +1,11 @@
 class Statement
   attr_reader :transactions
-  
+
   def initialize
     @transactions = []
+  end
+
+  def store_transaction(amount, balance)
+    @transactions.push(date: Time.now.strftime("%d/%m/%Y"), amount: amount, balance: balance)
   end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -2,8 +2,15 @@ require 'statement'
 
 describe Statement do
   let(:statement) { Statement.new }
+  let(:date) { Time.now.strftime("%d/%m/%Y") }
 
   it 'stores the transactions in an array' do
     expect(statement.transactions).to eq([])
+  end
+
+  it 'stores multiple transactions' do
+    statement.store_transaction(100, 100)
+    statement.store_transaction(400, 500)
+    expect(statement.transactions).to eq([{date: date, amount: 100, balance: 100}, {date: date, amount: 400, balance: 500}])
   end
 end
