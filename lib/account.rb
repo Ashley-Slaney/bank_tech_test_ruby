7ddc288df57869893ac9d3c0675efb234e44ac90
Ashley Slaney <ashleyslaney96@gmail.com>
2021-08-08 15:28:56 +0100

    added transactions logic to account class

--- lib/account.rb
+++ lib/account.rb
@@ -1,8 +1,9 @@
 class Account
-  attr_reader :balance 
+  attr_reader :balance, :transactions
 
   def initialize
     @balance = 0.00
+    @transactions = []
   end
 
   # returns the current balance
@@ -17,6 +18,7 @@ class Account
       return "Minimum deposit amount: £1"
     end
     @balance += amount
+    store_transaction("#{'%.2f' % amount} ||", @balance)
     @balance
   end
 
@@ -27,6 +29,21 @@ class Account
       return "Can not go below £0 balance"
     end
     @balance -= amount
+    store_transaction("|| #{'%.2f' % amount}", balance)
     @balance
+  end
+  
+  # pushes each transaction to the array that all of the transactions are being stored in
+  def store_transaction(amount, balance)
+    @transactions.push(date: Time.now.strftime("%d/%m/%Y"), amount: amount, balance: balance)
+  end
+
+  # rearranges the transactions array to show the most recent first
+  def organise_transactions
+    @statement_output = "date || credit || debit || balance"
+    @transactions.reverse.each do |transaction|
+      @statement_output << "\n#{transaction[:date]} || #{transaction[:amount]} || #{'%.2f' % transaction[:balance]}"
+    end
+    @statement_output
   end
 end
--- spec/unit/account_spec.rb
+++ spec/unit/account_spec.rb
@@ -2,6 +2,7 @@ require 'account'
 
 describe Account do
   let(:account) { Account.new }
+  let(:date) { Time.now.strftime("%d/%m/%Y") }
 
   it 'starts with 0 balance' do
     expect(account.balance).to eq(0.00)
@@ -41,5 +42,18 @@ describe Account do
 
   it 'can not go below £0 when withdrawing money' do
     expect(account.withdraw(10)).to eq("Can not go below £0 balance")
+  end
+
+  it 'can store transactions' do
+    account.deposit(100)
+    expect(account.transactions).to eq([{:amount=>"100.00 ||", :balance=>100.00, :date=>"#{date}"}])
+  end
+
+  it 'can return transactions with the most recent first' do
+    account.deposit(1000)
+    account.deposit(2000)
+    account.withdraw(500)
+    expect(account.transactions).to eq([{:amount=>"1000.00 ||", :balance=>1000.0, :date=>"#{date}"}, {:amount=>"2000.00 ||", :balance=>3000.0, :date=>"#{date}"}, {:amount=>"|| 500.00", :balance=>2500.0, :date=>"#{date}"}])
+    expect(account.organise_transactions).to eq("date || credit || debit || balance\n#{date} || || 500.00 || 2500.00\n#{date} || 2000.00 || || 3000.00\n#{date} || 1000.00 || || 1000.00")
   end
 end
