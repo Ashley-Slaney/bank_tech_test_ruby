7cdba4ea1f314d7d078d76e7c695792d21a6b07c
Ashley Slaney <ashleyslaney96@gmail.com>
2021-08-08 15:20:01 +0100

    removed transaction storing logic from statement class

--- lib/statement.rb
+++ lib/statement.rb
@@ -6,26 +6,10 @@ class Statement
   end
 
   # responsible for outputting each transaction 
-  def print_statement
-    organise_transactions
-    
-    output = @statement_output.split("\n")
+  def print_statement(transactions)    
+    output = transactions.split("\n")
     output.each do |transaction|
       puts transaction
-    end
-    @statement_output
-  end
-
-  # pushes each transaction to the array that all of the transactions are being stored in
-  def store_transaction(amount, balance)
-    @transactions.push(date: Time.now.strftime("%d/%m/%Y"), amount: amount, balance: balance)
-  end
-
-  # rearranges the transactions array to show the most recent first
-  def organise_transactions
-    @statement_output = "date || credit || debit || balance"
-    @transactions.reverse.each do |transaction|
-      @statement_output << "\n#{transaction[:date]} || #{transaction[:amount]} || #{'%.2f' % transaction[:balance]}"
     end
   end
 
--- spec/unit/statement_spec.rb
+++ spec/unit/statement_spec.rb
@@ -3,34 +3,9 @@ require 'statement'
 describe Statement do
   let(:statement) { Statement.new }
   let(:date) { Time.now.strftime("%d/%m/%Y") }
+  let(:transaction) { "date || credit || debit || balance\nsome_transaction" }
 
-  it 'stores the transactions in an array' do
-    expect(statement.transactions).to eq([])
-  end
-
-  it 'stores multiple transactions' do
-    statement.send(:store_transaction, 100, 100)
-    statement.send(:store_transaction, 400, 500)
-    statement.send(:store_transaction, 500, 1000)
-    expect(statement.transactions).to eq([{date: date, amount: 100, balance: 100},
-                                          {date: date, amount: 400, balance: 500},
-                                          {date: date, amount: 500, balance: 1000}])
-  end
-
-  it 'prints the statement' do
-    expect(statement.print_statement).to eq("date || credit || debit || balance")
-  end
-
-  it 'prints the statement after some transactions' do
-    statement.send(:store_transaction, "#{'%.2f' % 100} ||", 100)
-    statement.send(:store_transaction, "#{'%.2f' % 400} ||", 500)
-    statement.send(:store_transaction, "|| #{'%.2f' % 500}", 1000)
-    expect(statement.print_statement).to eq("date || credit || debit || balance\n#{date} || || 500.00 || 1000.00\n#{date} || 400.00 || || 500.00\n#{date} || 100.00 || || 100.00")
-  end
-
-  it 'does not duplicate the statement when calling for the statement consecutively' do
-    statement.print_statement
-    statement.print_statement
-    expect(statement.print_statement).to eq("date || credit || debit || balance")
+  it 'can print a bank statement' do
+    expect{ statement.print_statement(transaction) }.to output("date || credit || debit || balance\nsome_transaction\n").to_stdout
   end
 end
